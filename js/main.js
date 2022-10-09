9d4e66f82229088d644d54c509269e476dff8580
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-10-10 01:32:40 +0700

    Big Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
     $('#titleWeb').text(CONFIG.titleWeb)
     $('body').css('background-image', 'url(' + CONFIG.background + ')')
+
+    for (let i = 1; i <= 6; i++)
+        $('#min' + i).css('background-image', 'url(' + CONFIG['min' + i] + ')')
+
+    for (let i = 1; i <= 6; i++)
+        $('#max' + i).css('background-image', 'url(' + CONFIG['max' + i] + ')')
 })
