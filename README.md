b184d06081d9885b3bcf531b7b953c3b59cb1cf5
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-10-10 02:04:24 +0700

    Update

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-## <p align="center"> Web box </p>
-
-#### <p align="center"> [View Website](https://zukahai.github.io/webDemo-Box/) </p>
-[<p align="center"> <img src="https://github.com/zukahai/HaiZuka/blob/master/Images/WebBox/background.gif" alt="web" /> </p>](https://zukahai.github.io/webDemo-Box/)
+https://lytuong.net/upload-anh-online/
--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const CONFIG = {
 
     background: "https://img.freepik.com/free-photo/grunge-paint-background_1409-1337.jpg?w=2000",
 
-    min1: "https://previews.123rf.com/images/triken/triken1608/triken160800029/61320775-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg",
-    min2: "https://www.pngitem.com/pimgs/m/421-4212266_transparent-default-avatar-png-default-avatar-images-png.png",
+    min1: "https://gcdnb.pbrd.co/images/KNLRMesPM42q.jpg",
+    min2: "https://gcdnb.pbrd.co/images/Rkt08RMle58R.png",
     min3: "https://www.pngitem.com/pimgs/m/421-4212266_transparent-default-avatar-png-default-avatar-images-png.png",
     min4: "https://www.pngitem.com/pimgs/m/421-4212266_transparent-default-avatar-png-default-avatar-images-png.png",
     min5: "https://www.pngitem.com/pimgs/m/421-4212266_transparent-default-avatar-png-default-avatar-images-png.png",
