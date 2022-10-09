5ac28846214d81d591dfbcfb472a3c79e1a84a1b
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2022-10-09 14:14:36 +0700

    Update Background

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web2",
+    background: "https://img.freepik.com/free-photo/grunge-paint-background_1409-1337.jpg?w=2000",
+}
---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    $('#titleWeb').text(CONFIG.titleWeb)
+    $('body').css('background-image', 'url(' + CONFIG.background + ')')
+})
